946924346e3054601ba0225ff47e3700709da30b
Ramin <ramin_n11@icloud.com>
2023-09-27 20:31:07 +0300

    Выполнил дз

---
+++ exercise2/Program.cs
@@ -0,0 +1,61 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+//Console.WriteLine("Введите чиcло");
+//int number = int.Parse(Console.ReadLine());
+//int thirdDigit = number / 10;
+//
+//if (number < 100)
+//{
+  //  Console.WriteLine("Третье цифры нет");
+//}
+//else 
+//{
+  //  Console.WriteLine($"Третья цифра числа = {thirdDigit}");
+//}
+
+
+
+
+
+
+
+
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+
+
+// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
+
+
+
+
